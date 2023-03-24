6a64b9f968bbbc06c220ca1d4e5380c94412e74a
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-24 11:25:25 +0530

    initial commit

---
+++ src/main/java/com/boot/practice/bootPractice/BootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.boot.practice.bootPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPracticeApplication.class, args);
+	}
+
+}
